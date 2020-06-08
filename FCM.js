1d05d3faa7ee7ffc76993e20dabb746e9ef75ae6
ZachyDev <mosetizachary001@gmail.com>
2020-06-08 16:31:50 +0300

    Getting started with Firebase Cloud Messaging

---
+++ FCM.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyD1iMd26sRM5QpvYmH4o_9rVr6a7K6ndXw",
+    authDomain: "fir-messaging-2a230.firebaseapp.com",
+    databaseURL: "https://fir-messaging-2a230.firebaseio.com",
+    projectId: "fir-messaging-2a230",
+    storageBucket: "fir-messaging-2a230.appspot.com",
+    messagingSenderId: "984600315066",
+    appId: "1:984600315066:web:ee48bf99087d783bdeae83"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
